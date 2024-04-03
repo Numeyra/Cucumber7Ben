857c0e9fd9478b445a2ae5dad06dce8b75c25a8a
Numeyra73 <numeyra122@icloud.com>
2024-04-03 18:25:35 +0200

    XML File eklendi

--- src/test/java/Runners/_08_ParallelTest.java
+++ src/test/java/Runners/_08_ParallelTest.java
@@ -1,21 +1,25 @@
 package Runners;
 
+
+import Utilities.GWD;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 @CucumberOptions(
-
         tags = "@Regression",
         features = {"src/test/java/FeatureFiles"},
-        glue ={"StepDefinitions"},
+        glue = {"StepDefinitions"},
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
+public class _08_ParallelTest extends AbstractTestNGCucumberTests {
 
 
 
 
 
 
-public class _08_ParalelTest extends AbstractTestNGCucumberTests {
+
+
 }
